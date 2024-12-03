cc5e21f110a03515c1c638f608be0e68914bf05d
dakalomunonde <86750102+psyke47@users.noreply.github.com>
2024-12-03 09:28:16 +0200

    Reading Progress Bar

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript to handle the scroll progress bar
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+        var scrollTop = $(window).scrollTop();
+        var docHeight = $(document).height();
+        var winHeight = $(window).height();
+        var scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
+        $('#progressBar div').css('width', scrollPercent + '%');
+    });
+});
